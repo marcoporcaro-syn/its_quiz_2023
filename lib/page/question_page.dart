a0b219506e0de63d128ee77ef39ed742b7c688cf
Marco <marco.porcaro@synesthesia.it>
2023-06-15 11:26:36 +0200

    Go to next question automatically after 3 seconds

--- lib/page/question_page.dart
+++ lib/page/question_page.dart
@@ -13,40 +13,26 @@ class QuestionPage extends StatefulWidget {
 }
 
 class _QuestionPageState extends State<QuestionPage> {
+  final _futureQuestions = QuestionRepository().getQuestions();
+  String? _givenAnswer;
+  int _questionIndex = 0;
+  int _questionSize = 0;
 
-  final futureQuestions = QuestionRepository().getQuestions();
-
-  String? givenAnswer;
-  int questionIndex = 0;
-
-  pickAnswer(givenAnswer) {
+  _pickAnswer(givenAnswer) {
     setState(() {
-      this.givenAnswer = givenAnswer;
+      _givenAnswer = givenAnswer;
     });
+    _goToNextQuestion();
   }
 
-  goToNextAnswer() {
+  _goToNextQuestion() async {
+    await Future.delayed(const Duration(seconds: 3));
     setState(() {
-      if (givenAnswer != null) {
-        questionIndex++;
-        givenAnswer = null;
+      if (_givenAnswer != null && _questionIndex < _questionSize - 1) {
+        _questionIndex++;
+        _givenAnswer = null;
       }
     });
-  }
-
-  goToPreviousAnswer() {
-    setState(() {
-      if (givenAnswer != null) {
-        questionIndex--;
-        givenAnswer = null;
-      }
-    });
-  }
-
-  @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
   }
 
   @override
@@ -63,96 +49,74 @@ class _QuestionPageState extends State<QuestionPage> {
           child: Padding(
             padding: const EdgeInsets.symmetric(horizontal: 20),
             child: FutureBuilder<List<Question>>(
-              future: futureQuestions,
-              builder: (BuildContext context, AsyncSnapshot<List<Question>> snapshot) {
+                future: _futureQuestions,
+                builder: (BuildContext context, AsyncSnapshot<List<Question>> snapshot) {
+                  debugPrint("snapshot: ${snapshot.connectionState}");
+                  //while waiting for data
+                  if (snapshot.connectionState != ConnectionState.done) {
+                    return const Center(child: CircularProgressIndicator());
+                  }
 
-                debugPrint("snapshot: ${snapshot.connectionState}");
-                //while waiting for data
-                if(snapshot.connectionState != ConnectionState.done) {
-                  return const Center(child: CircularProgressIndicator());
-                }
+                  if (snapshot.hasError) {
+                    return const Center(child: Text("Error while loading data", style: questionTextStyle));
+                  }
 
-                if(snapshot.hasError) {
-                  return const Center(child: Text(
-                      "Error while loading data",
-                      style: questionTextStyle));
-                }
-
-                final questionList = snapshot.data;
-                if(questionList == null) {
-                  return const Center(child: Text("No questions found ",
-                  style: questionTextStyle,));
-                }
-
-                final currentQuestion = questionList[questionIndex];
-                return Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Image.asset(
-                        "assets/images/logo2.png",
-                        height: 100,
-                      ),
-                    ),
-                    Text(
-                      currentQuestion.text,
+                  final questionList = snapshot.data;
+                  if (questionList == null) {
+                    return const Center(
+                        child: Text(
+                      "No questions found ",
                       style: questionTextStyle,
-                      textAlign: TextAlign.center,
-                    ),
-                    const SizedBox(height: 30),
-                    Expanded(
-                      child: Container(
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(20),
+                    ));
+                  }
+
+                  _questionSize = questionList.length;
+                  final currentQuestion = questionList[_questionIndex];
+                  return Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Image.asset(
+                          "assets/images/logo2.png",
+                          height: 100,
                         ),
-                        clipBehavior: Clip.hardEdge,
-                        child: Image.network(currentQuestion.imageUrl),
                       ),
-                    ),
-                    const SizedBox(height: 20),
-                    ...currentQuestion.answers.map((singleAnswer) {
-                      var buttonStatus = AnswerButtonStatus.NOT_ANSWERED;
-                      if (givenAnswer != null) {
-                        if (singleAnswer == currentQuestion.rightAnswer) {
-                          buttonStatus = AnswerButtonStatus.RIGHT;
-                        }
-                        if (singleAnswer == givenAnswer && givenAnswer != currentQuestion.rightAnswer) {
-                          buttonStatus = AnswerButtonStatus.WRONG;
-                        }
-                      }
-
-                      return AnswerButton(
-                          text: singleAnswer, status: buttonStatus, onClick: () => givenAnswer == null ? pickAnswer(singleAnswer) : null);
-                    }).toList(),
-                    const SizedBox(height: 20),
-                    Row(
-                      children: [
-                        if (questionIndex > 0)
-                          ElevatedButton(
-                              onPressed: () {
-                                goToPreviousAnswer();
-                              },
-                              child: const Text("Indietro")
+                      Text(
+                        currentQuestion.text,
+                        style: questionTextStyle,
+                        textAlign: TextAlign.center,
+                      ),
+                      const SizedBox(height: 30),
+                      Expanded(
+                        child: Container(
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(20),
                           ),
-                        Expanded(
-                          child: Container(),
+                          clipBehavior: Clip.hardEdge,
+                          child: Image.network(currentQuestion.imageUrl),
                         ),
-                        if(questionIndex < questionList.length - 1)
-                          ElevatedButton(
-                              onPressed: () {
-                                goToNextAnswer();
-                              },
-                              child: const Text("Avanti")
-                          )
-                      ],
-                    )
-                  ],
-                );
+                      ),
+                      const SizedBox(height: 20),
+                      ...currentQuestion.answers.map((singleAnswer) {
+                        var buttonStatus = AnswerButtonStatus.NOT_ANSWERED;
+                        if (_givenAnswer != null) {
+                          if (singleAnswer == currentQuestion.rightAnswer) {
+                            buttonStatus = AnswerButtonStatus.RIGHT;
+                          }
+                          if (singleAnswer == _givenAnswer && _givenAnswer != currentQuestion.rightAnswer) {
+                            buttonStatus = AnswerButtonStatus.WRONG;
+                          }
+                        }
 
-              }
-            ),
+                        return AnswerButton(
+                            text: singleAnswer, status: buttonStatus, onClick: () => _givenAnswer == null ? _pickAnswer(singleAnswer) : null);
+                      }).toList(),
+                      const SizedBox(height: 20),
+                    ],
+                  );
+                }),
           ),
         ),
       ),
